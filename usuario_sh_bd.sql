8303b3775fd518a807b94e1258006c8565bd004c
Thiago Luz <engs-thiagoluz@camporeal.edu.br>
2026-09-14 11:33:35 -0300

    Usuario atualizado

--- usuario_sh_bd.sql
+++ usuario_sh_bd.sql
@@ -1,14 +1,9 @@
-
-set search_path to thiago_sistemahospitalar_bd;
-
 CREATE ROLE readonly PASSWORD 'Senhaboa123!';
-
-GRANT CONNECT ON DATABASE thiago_sistema_hospitalar_bd TO readonly;  
-
-GRANT USAGE ON SCHEMA thiago_sistemahospitalar_bd TO readonly;       
-
+GRANT CONNECT ON DATABASE thiago_sistema_hospitalar_bd TO readonly;
+GRANT USAGE ON SCHEMA public TO readonly;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
-
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO readonly;
 CREATE ROLE professor WITH LOGIN PASSWORD 'Senhaboa456!';
-
 GRANT readonly TO professor;
