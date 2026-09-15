c5fde684def93ed47450b43cd6449fa21bdc797e
Thiago Luz <engs-thiagoluz@camporeal.edu.br>
2026-09-15 20:52:13 -0300

    Correção definitiva do usuário

--- usuario_sh_bd.sql
+++ usuario_sh_bd.sql
@@ -1,9 +1,19 @@
-CREATE ROLE readonly PASSWORD 'Senhaboa123!';
+
+
 GRANT CONNECT ON DATABASE thiago_sistema_hospitalar_bd TO readonly;
-GRANT USAGE ON SCHEMA public TO readonly;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO readonly;
-CREATE ROLE professor WITH LOGIN PASSWORD 'Senhaboa456!';
-GRANT readonly TO professor;
+
+GRANT USAGE ON SCHEMA thiago_sistemahospitalar_bd TO readonly;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA thiago_sistemahospitalar_bd TO readonly;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA thiago_sistemahospitalar_bd
+    GRANT SELECT ON TABLES TO readonly;
+
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA thiago_sistemahospitalar_bd TO readonly;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA thiago_sistemahospitalar_bd
+    GRANT SELECT ON SEQUENCES TO readonly;
+
+ALTER ROLE professor SET search_path TO thiago_sistemahospitalar_bd;
+
+-- Connection string: postgresql://professor:Senhaboa456%21@ep-spring-waterfall-ac0qs29b-pooler.sa-east-1.aws.neon.tech/thiago_sistema_hospitalar_bd?sslmode=require&channel_binding=require
